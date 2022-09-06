21e79cd27d36a026ead1d6166dd98cb68ddbd448
Madeiradoradal <101781189+Madeiradoradal@users.noreply.github.com>
2022-09-06 11:49:48 -0500

    Add files via upload

---
+++ precache-manifest.00261da540ede3bfdb68d0b54c873b96.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e14bbc388425f942142a65e51a93633",
+    "url": "/MadeiraHotel/index.html"
+  },
+  {
+    "revision": "94dde37407263c2340dc",
+    "url": "/MadeiraHotel/static/css/2.ede86a65.chunk.css"
+  },
+  {
+    "revision": "ee54e910a5328fd83396",
+    "url": "/MadeiraHotel/static/css/main.b73b0535.chunk.css"
+  },
+  {
+    "revision": "94dde37407263c2340dc",
+    "url": "/MadeiraHotel/static/js/2.35988265.chunk.js"
+  },
+  {
+    "revision": "ec15c8cbec563da3ac9eea5cd64d9660",
+    "url": "/MadeiraHotel/static/js/2.35988265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee54e910a5328fd83396",
+    "url": "/MadeiraHotel/static/js/main.89808f1e.chunk.js"
+  },
+  {
+    "revision": "948155258b9545a65f6c",
+    "url": "/MadeiraHotel/static/js/runtime-main.f79d7c26.js"
+  },
+  {
+    "revision": "c783c37bb06ece70f86418cc47b59e39",
+    "url": "/MadeiraHotel/static/media/DSC_7890-HDR.c783c37b.jpg"
+  },
+  {
+    "revision": "8facb6cb729cfb6fff3ad29e94f2a5fb",
+    "url": "/MadeiraHotel/static/media/DSC_7894-HDR-Editar.8facb6cb.jpg"
+  },
+  {
+    "revision": "fd4cacaa0f75d15e753d340d99cac758",
+    "url": "/MadeiraHotel/static/media/DSC_7914-HDR-Editar.fd4cacaa.jpg"
+  },
+  {
+    "revision": "85c6147eda94985abf348c1189ee3578",
+    "url": "/MadeiraHotel/static/media/DSC_8058-HDR-Editar.85c6147e.jpg"
+  },
+  {
+    "revision": "164a78bc0a0ad4ab7dcfbda74b047d00",
+    "url": "/MadeiraHotel/static/media/DSC_8122-HDR-Editar.164a78bc.jpg"
+  },
+  {
+    "revision": "92392d15b69db883f26fbdebc5015b46",
+    "url": "/MadeiraHotel/static/media/DSC_8128-HDR-Editar.92392d15.jpg"
+  },
+  {
+    "revision": "c6ed81dd7b049ea2040f1300cb78060a",
+    "url": "/MadeiraHotel/static/media/about.c6ed81dd.svg"
+  },
+  {
+    "revision": "e65a6536fd277930d0d7eeccd7dfb89f",
+    "url": "/MadeiraHotel/static/media/customer.e65a6536.svg"
+  },
+  {
+    "revision": "f6f05af4ebfd528b999e303df5a90263",
+    "url": "/MadeiraHotel/static/media/customer1.f6f05af4.svg"
+  },
+  {
+    "revision": "9dac55866191ecfd805f015323ccf807",
+    "url": "/MadeiraHotel/static/media/customer3.9dac5586.svg"
+  },
+  {
+    "revision": "d1e8cb2d89d663e971662c51c0df9963",
+    "url": "/MadeiraHotel/static/media/en.d1e8cb2d.svg"
+  },
+  {
+    "revision": "6edb36f3feccea6f8483d2808828b6e1",
+    "url": "/MadeiraHotel/static/media/error.6edb36f3.jpg"
+  },
+  {
+    "revision": "c193004b3f55b9338bc1b3c9692f4744",
+    "url": "/MadeiraHotel/static/media/es.c193004b.svg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/MadeiraHotel/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "/MadeiraHotel/static/media/notfound.8c3d2a79.svg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/MadeiraHotel/static/media/room-3.9a1103b5.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ed857c714d1b3e72cb866381792266d.js"
+  "/MadeiraHotel/precache-manifest.00261da540ede3bfdb68d0b54c873b96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MadeiraHotel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
